4c710e555695a2481dc00f73336bf74745f28c51
Reddy Eada <Umamaheswara.Reddy@in.imshealth.com>
2019-08-12 14:25:12 +0530

    This is my first git commit. Docker With Micro Services committed

---
+++ DockerWithMicroServices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerWithMicroServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
